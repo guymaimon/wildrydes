9c4a20db27ab87836b662662e3c91681e1f3a93d
guymaimon <guymaimon90@walla.com>
2025-06-23 22:17:50 +0300

    Configure API invokeURL

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ module.exports = {
         region: 'eu-west-1'
     },
     api: {
-        invokeUrl: 'https://5z4o1y3b5i.execute-api.eu-west-1.amazonaws.com/prod'
+        invokeUrl: 'https://9k3dhaj1vb.execute-api.eu-west-1.amazonaws.com/prod'
     }
 };
 
