be020b914b54a6a9b56606718f87763174375b17
guymaimon <guymaimon90@walla.com>
2025-06-23 19:33:40 +0300

    Add correct config.js with invokeUrl

---
+++ src/config.js
@@ -0,0 +1,16 @@
+// Add your API invoke URL below, configured in Module 3. 
+
+// src/config.js
+
+module.exports = {
+    cognito: {
+        userPoolId: 'eu-west-1_jVH32930O',             // ודא שאלו הערכים האמיתיים שלך מ-AWS Cognito
+        userPoolClientId: '2tpp7cjnvohbspt0nd4d8ul55d', // אותו כנ"ל
+        region: 'eu-west-1'
+    },
+    api: {
+        invokeUrl: 'https://5z4o1y3b5i.execute-api.eu-west-1.amazonaws.com/prod'
+    }
+};
+
+
