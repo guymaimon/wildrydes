b8b4c584400c7839d631838bfa2af758f0391571
guymaimon <guymaimon90@walla.com>
2025-06-23 20:28:35 +0300

    Fix unicorn request logic and handle invalid API responses

--- src/components/map.vue
+++ src/components/map.vue
@@ -5,7 +5,7 @@
 <script>
 import { loadModules } from 'esri-loader'
 import axios from 'axios'
-import { api } from '../config.js' // ייבוא נכון מהקובץ src/config.js
+import { api } from '../config.js' // Make sure config.js exports `api` properly
 
 export default {
   name: 'mapView',
@@ -15,7 +15,6 @@ export default {
   data() {
     return {
       webmap: {},
-      authToken: this.token,
     }
   },
   mounted() {
@@ -29,98 +28,80 @@ export default {
       'esri/geometry/support/webMercatorUtils',
       'esri/geometry/Point',
       'esri/core/watchUtils',
-    ])
-      .then(
-        ([
-          MapView,
-          WebMap,
-          TextSymbol,
-          Graphic,
-          PictureMarkerSymbol,
-          webMercatorUtils,
-          Point,
-          watchUtils,
-        ]) => {
-          self.webmap = new WebMap({ basemap: 'gray-vector' })
+    ]).then(([
+      MapView,
+      WebMap,
+      TextSymbol,
+      Graphic,
+      PictureMarkerSymbol,
+      webMercatorUtils,
+      Point,
+      watchUtils,
+    ]) => {
+      self.webmap = new WebMap({ basemap: 'gray-vector' })
 
-          const view = new MapView({
-            center: [-122.31, 47.6],
-            container: 'viewDiv',
-            map: self.webmap,
-            zoom: 12,
-          })
-
-          const pinSymbol = new TextSymbol({
-            color: '#f50856',
-            text: '\ue61d',
-            font: {
-              size: 20,
-              family: 'CalciteWebCoreIcons',
-            },
-          })
-
-          const unicornSymbol = new PictureMarkerSymbol({
-            url: '/images/unicorn-icon.png',
-            width: '25px',
-            height: '25px',
-          })
-
-          let pinGraphic
-          let unicornGraphic
-
-          watchUtils.watch(view, 'center', (center) => {
-            self.webmap.center = center.clone().toJSON()
-          })
-
-          watchUtils.watch(view, 'extent', (extent) => {
-            self.webmap.extent = extent.clone().toJSON()
-          })
-
-          view.on('click', function handleViewClick(event) {
-            self.webmap.selectedPoint = event.mapPoint
-            view.graphics.remove(pinGraphic)
-            pinGraphic = new Graphic({
-              symbol: pinSymbol,
-              geometry: self.webmap.selectedPoint,
-            })
-            view.graphics.add(pinGraphic)
-            self.enableRequest(self.webmap)
-          })
-
-          self.webmap.animate = function animate(origin, dest, callback) {
-            let startTime
-            const step = function animateFrame(timestamp) {
-              if (!startTime) startTime = timestamp
-              const progress = timestamp - startTime
-              const progressPct = Math.min(progress / 2000, 1)
-
-              const deltaLat = (dest.latitude - origin.latitude) * progressPct
-              const deltaLon = (dest.longitude - origin.longitude) * progressPct
-              const point = new Point({
-                longitude: origin.longitude + deltaLon,
-                latitude: origin.latitude + deltaLat,
-              })
-
-              view.graphics.remove(unicornGraphic)
-              unicornGraphic = new Graphic({
-                geometry: point,
-                symbol: unicornSymbol,
-              })
-              view.graphics.add(unicornGraphic)
-
-              if (progressPct < 1) {
-                requestAnimationFrame(step)
-              } else {
-                callback()
-              }
-            }
-            requestAnimationFrame(step)
-          }
-        }
-      )
-      .catch((err) => {
-        console.error(err)
+      const view = new MapView({
+        center: [-122.31, 47.6],
+        container: 'viewDiv',
+        map: self.webmap,
+        zoom: 12,
       })
+
+      const pinSymbol = new TextSymbol({
+        color: '#f50856',
+        text: '\ue61d',
+        font: { size: 20, family: 'CalciteWebCoreIcons' },
+      })
+
+      const unicornSymbol = new PictureMarkerSymbol({
+        url: '/images/unicorn-icon.png',
+        width: '25px',
+        height: '25px',
+      })
+
+      let pinGraphic, unicornGraphic
+
+      watchUtils.watch(view, 'center', (center) => {
+        self.webmap.center = center.clone().toJSON()
+      })
+
+      watchUtils.watch(view, 'extent', (extent) => {
+        self.webmap.extent = extent.clone().toJSON()
+      })
+
+      view.on('click', (event) => {
+        self.webmap.selectedPoint = event.mapPoint
+        view.graphics.remove(pinGraphic)
+        pinGraphic = new Graphic({
+          symbol: pinSymbol,
+          geometry: self.webmap.selectedPoint,
+        })
+        view.graphics.add(pinGraphic)
+        self.enableRequest(self.webmap)
+      })
+
+      self.webmap.animate = function (origin, dest, callback) {
+        let startTime
+        const step = function (timestamp) {
+          if (!startTime) startTime = timestamp
+          const progress = timestamp - startTime
+          const pct = Math.min(progress / 2000, 1)
+
+          const point = new Point({
+            longitude: origin.longitude + (dest.longitude - origin.longitude) * pct,
+            latitude: origin.latitude + (dest.latitude - origin.latitude) * pct,
+          })
+
+          view.graphics.remove(unicornGraphic)
+          unicornGraphic = new Graphic({ geometry: point, symbol: unicornSymbol })
+          view.graphics.add(unicornGraphic)
+
+          if (pct < 1) requestAnimationFrame(step)
+          else callback()
+        }
+        requestAnimationFrame(step)
+      }
+    }).catch((err) => console.error(err))
   },
   methods: {
     enableRequest(ev) {
@@ -131,12 +112,11 @@ export default {
       this.requestUnicorn(this.webmap.selectedPoint)
     },
     requestUnicorn(pickupLocation) {
-      const self = this
       axios({
         method: 'POST',
-        url: api.invokeUrl + '/ride',
+        url: `${api.invokeUrl}/ride`,
         headers: {
-          Authorization: self.token,
+          Authorization: this.token,
           'Content-Type': 'application/json',
         },
         data: {
@@ -147,7 +127,8 @@ export default {
         },
       })
         .then((response) => {
-          self.completeRequest(response.data)
+          console.log('Full API Response:', response)
+          this.completeRequest(response.data)
         })
         .catch((error) => {
           console.error('Error requesting ride: ', error)
@@ -164,9 +145,7 @@ export default {
 
       const unicorn = unicornResponse.Unicorn
       const pronoun = unicorn.Gender === 'Male' ? 'his' : 'her'
-      this.displayUpdate(
-        `${unicorn.Name}, your ${unicorn.Color} unicorn, is on ${pronoun} way.`
-      )
+      this.displayUpdate(`${unicorn.Name}, your ${unicorn.Color} unicorn, is on ${pronoun} way.`)
 
       this.animateArrival(() => {
         this.displayUpdate(`${unicorn.Name} has arrived. Giddy up!`)
@@ -178,17 +157,10 @@ export default {
     },
     animateArrival(callback) {
       const dest = this.webmap.selectedPoint
-      const origin = {}
-
-      origin.latitude =
-        dest.latitude > this.webmap.center.latitude
-          ? this.webmap.extent.minLat
-          : this.webmap.extent.maxLat
-
-      origin.longitude =
-        dest.longitude > this.webmap.center.longitude
-          ? this.webmap.extent.minLng
-          : this.webmap.extent.maxLng
+      const origin = {
+        latitude: dest.latitude > this.webmap.center.latitude ? this.webmap.extent.minLat : this.webmap.extent.maxLat,
+        longitude: dest.longitude > this.webmap.center.longitude ? this.webmap.extent.minLng : this.webmap.extent.maxLng,
+      }
 
       this.webmap.animate(origin, dest, callback)
     },
