816ee5d2379623027db8bd3c591612141671af70
guymaimon <guymaimon90@walla.com>
2025-06-23 22:45:23 +0300

    regular

--- src/config.js
+++ src/config.js
@@ -1,16 +1,7 @@
 // Add your API invoke URL below, configured in Module 3. 
 
-// src/config.js
-
 module.exports = {
-    cognito: {
-        userPoolId: 'eu-west-1_jVH32930O',             // ודא שאלו הערכים האמיתיים שלך מ-AWS Cognito
-        userPoolClientId: '2tpp7cjnvohbspt0nd4d8ul55d', // אותו כנ"ל
-        region: 'eu-west-1'
-    },
     api: {
-        invokeUrl: 'https://9k3dhaj1vb.execute-api.eu-west-1.amazonaws.com/prod'
+        invokeUrl: 'https://9k3dhaj1vb.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
-};
-
-
+}
