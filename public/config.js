6b447d5907fe54312d7a153e26e8809761b02c8f
guymaimon <60132246+guymaimon@users.noreply.github.com>
2025-06-23 19:17:38 +0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,13 +1,12 @@
-// Add your API invoke URL below, configured in Module 3. 
+// public/config.js
 
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_jVH32930O', // ודא שאלה הערכים שלך
+        userPoolId: 'eu-west-1_jVH32930O',
         userPoolClientId: '2tpp7cjnvohbspt0nd4d8ul55d',
         region: 'eu-west-1'
     },
     api: {
-        invokeUrl: 'https://5z4o1y3b5i.execute-api.eu-west-1.amazonaws.com/prod' // כאן שמים את ה-Invoke URL החדש
+        invokeUrl: 'https://5z4o1y3b5i.execute-api.eu-west-1.amazonaws.com/prod'
     }
 };
-
